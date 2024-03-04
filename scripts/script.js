2d585f3ba447f7974b393f19528514fa3fd01fc7
Loes_s <103633598+LoesSsssSssssssssssssssssssss@users.noreply.github.com>
2024-03-04 23:16:59 +0300

    update

--- scripts/script.js
+++ scripts/script.js
@@ -24,3 +24,60 @@ document.querySelectorAll('.accord_question_header').forEach((el) => {
     }
   });
 });
+
+const btns = document.querySelectorAll('.header_btn');
+const modalOverlay = document.querySelector('.modal-overlay ');
+const modals = document.querySelectorAll('.modal');
+const auth = document.querySelectorAll('.svap');
+const reg = document.querySelectorAll('.svap2');
+const content = document.querySelectorAll('.modal_content_reg');
+const contentreg = document.querySelectorAll('.modal_content_auth');
+
+btns.forEach((el) => {
+  el.addEventListener('click', (e) => {
+    let path = e.currentTarget.getAttribute('data-path');
+
+    modals.forEach((el) => {
+      el.classList.remove('modal--visible');
+    });
+
+    document
+      .querySelector(`[data-target="${path}"]`)
+      .classList.add('modal--visible');
+    modalOverlay.classList.add('modal-overlay--visible');
+  });
+});
+
+auth.forEach((el) => {
+  el.addEventListener('click', (e) => {
+    content.forEach((el) => {
+      el.classList.add('display_none');
+    });
+    contentreg.forEach((el) => {
+      el.classList.remove('display_none');
+    });
+  });
+});
+
+reg.forEach((el) => {
+  el.addEventListener('click', (e) => {
+    contentreg.forEach((el) => {
+      el.classList.add('display_none');
+    });
+    content.forEach((el) => {
+      el.classList.remove('display_none');
+    });
+  });
+});
+
+modalOverlay.addEventListener('click', (e) => {
+  console.log(e.target);
+
+  if (e.target == modalOverlay) {
+    modalOverlay.classList.remove('modal-overlay--visible');
+    modals.forEach((el) => {
+      el.classList.remove('modal--visible');
+      el.classList.remove('display_none');
+    });
+  }
+});
